ad825c7540b42c49c7e2c0800440247c0226d4c0
Benjamin Jones <bljones36@gmail.com>
2018-10-08 03:30:23 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,7 +1,6 @@
 module Players
   class Computer < Player
     def move(board)
-<<<<<<< HEAD
       sleep(2)
       count = 1
       valid = []
@@ -16,17 +15,6 @@ module Players
       else
       valid.sample
       end
-=======
-      count = 0
-      valid = []
-      board.cells.collect do |moves|
-        if moves == " "
-          count += 1
-          valid << count.to_s
-        end
-      end
-      valid.sample
->>>>>>> a23b2f99e67575e64ab41cee9ba285b75cc183fa
     end
   end
 end
